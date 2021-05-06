501b838f95ae4d0ff9bdefd3f0ece53997393de2
geoknee <georgeknee@googlemail.com>
2021-05-06 13:50:16 +0100

    sketch out how a nitro guaranteeOutcome works

---
+++ ts/nitro-types.ts
@@ -0,0 +1,33 @@
+import { defaultAbiCoder } from "@ethersproject/abi";
+import { constants } from "ethers";
+import { Exit } from "./types";
+
+const MAGIC_VALUE_DENOTING_A_GUARANTEE =
+  "0x0000000000000000000000000000000000000001";
+// this will cause executeExit to revert, which is what we want for a guarantee
+// it should only work with a custom 'claim' operation
+
+const guaranteeOutcome: Exit = [
+  {
+    asset: constants.AddressZero,
+    data: "0x",
+    allocations: [
+      {
+        destination: "0xjointchannel1",
+        amount: "0xa",
+        callTo: MAGIC_VALUE_DENOTING_A_GUARANTEE,
+        data: encodeGuarantee("0xAlice", "0xHarry", "0xvirtualchannel1"),
+      },
+      {
+        destination: "0xjointchannel2",
+        amount: "0xa",
+        callTo: MAGIC_VALUE_DENOTING_A_GUARANTEE,
+        data: encodeGuarantee("0xAlice", "0xHarry", "0xvirtualchannel2"),
+      },
+    ],
+  },
+];
+
+function encodeGuarantee(...destinations: string[]) {
+  return defaultAbiCoder.encode(["address[]"], destinations);
+}
