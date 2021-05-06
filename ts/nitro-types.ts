81a99f570d7423064dddeadf0c711b01c6f0d528
geoknee <georgeknee@googlemail.com>
2021-05-06 16:58:09 +0100

    introduce GuaranteeAllocation and GuaranteeOutcome
    types

--- ts/nitro-types.ts
+++ ts/nitro-types.ts
@@ -1,14 +1,24 @@
 import { defaultAbiCoder } from "@ethersproject/abi";
-import { constants } from "ethers";
-import { Exit } from "./types";
+import { BytesLike, constants } from "ethers";
+import { Exit, Allocation, SingleAssetExit } from "./types";
 
-const MAGIC_VALUE_DENOTING_A_GUARANTEE =
+export const MAGIC_VALUE_DENOTING_A_GUARANTEE =
   "0x0000000000000000000000000000000000000001";
 // this will cause executeExit to revert, which is what we want for a guarantee
 // it should only work with a custom 'claim' operation
 // we avoid the magic value of the zero address, because that is already used by executeExit
 
-const guaranteeOutcome: Exit = [
+export type GuaranteeAllocation = Allocation & {
+  callTo: typeof MAGIC_VALUE_DENOTING_A_GUARANTEE;
+};
+
+export type SingleAssetGuaranteeOutcome = SingleAssetExit & {
+  allocations: GuaranteeAllocation[];
+};
+
+export type GuaranteeOutcome = SingleAssetGuaranteeOutcome[];
+
+const exampleGuaranteeOutcome1: GuaranteeOutcome = [
   {
     asset: constants.AddressZero,
     data: "0x",
@@ -17,18 +27,20 @@ const guaranteeOutcome: Exit = [
         destination: "0xjointchannel1",
         amount: "0xa",
         callTo: MAGIC_VALUE_DENOTING_A_GUARANTEE,
-        data: encodeGuarantee("0xAlice", "0xHarry", "0xvirtualchannel1"),
+        data: encodeGuaranteeData("0xAlice", "0xHarry", "0xvirtualchannel1"),
       },
       {
         destination: "0xjointchannel2",
         amount: "0xa",
         callTo: MAGIC_VALUE_DENOTING_A_GUARANTEE,
-        data: encodeGuarantee("0xAlice", "0xHarry", "0xvirtualchannel2"),
+        data: encodeGuaranteeData("0xAlice", "0xHarry", "0xvirtualchannel2"),
       },
     ],
   },
 ];
 
-function encodeGuarantee(...destinations: string[]) {
+const exampleGuaranteeOutcome2: Exit = exampleGuaranteeOutcome1; // GuaranteeOutcome is assignable to Exit
+
+function encodeGuaranteeData(...destinations: string[]): BytesLike {
   return defaultAbiCoder.encode(["address[]"], destinations);
 }
