fada0d840a8a948c91d1a40c6aa44370952626b4
Colin Kennedy <colin@mrkennedy.ca>
2021-05-07 16:03:38 -0300

    chore: move files to flatten directory structure

--- cairo/cairo.sh
+++ cairo/cairo.sh
@@ -2,5 +2,5 @@
 
 cairo-compile $1 --output $1.json
 
-firefox "http://localhost:8100" # open the debugger
+# firefox "http://localhost:8100" # open the debugger
 cairo-run --program=$1.json --print_output --layout=small --program_input=$2 --tracer
