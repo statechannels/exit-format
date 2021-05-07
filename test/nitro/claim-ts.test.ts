c7462772bf35dccd48039d47746bbae80e96d964
Alex Gap <alex.gap@gmail.com>
2021-05-07 11:55:09 -0700

    add test for underderfunded scenario

--- test/nitro/claim-ts.test.ts
+++ test/nitro/claim-ts.test.ts
@@ -1,5 +1,5 @@
 const { expect } = require("chai");
-import { BigNumber } from "@ethersproject/bignumber";
+import { BigNumber, BigNumberish } from "@ethersproject/bignumber";
 import { claim } from "../../nitro-src/claim";
 import {
   encodeGuaranteeData,
@@ -13,6 +13,92 @@ describe("claim (typescript)", function () {
   const A_ADDRESS = "0x96f7123E3A80C9813eF50213ADEd0e4511CB820f";
   const B_ADDRESS = "0x53484E75151D07FfD885159d4CF014B874cd2810";
   const TARGET_CHANNEL_ADDRESS = "0x080678731247781ff0d57c649b6d0ad1a0620df0"; // At some point in the full claim operation, the outcome of this channel must be read and checked
+  const ANOTHER_TARGET_CHANNEL_ADDRESS =
+    "0x27592B3827907B54684E4f9da3d988263828893D"; // At some point in the full claim operation, the outcome of this channel must be read and checked
+  const I_ADDRESS = "0x7ab853663C531EaA080d84091AD0E0e985c688C7";
+
+  const createOutcome = (
+    allocations: ["A" | "B" | "I", BigNumberish][]
+  ): Exit => {
+    return [
+      {
+        asset: ZERO_ADDRESS,
+        data: "0x",
+        allocations: allocations.map((a) => ({
+          destination:
+            a[0] === "A" ? A_ADDRESS : a[0] === "B" ? B_ADDRESS : I_ADDRESS,
+          amount: BigNumber.from(a[1]).toHexString(),
+          callTo: ZERO_ADDRESS,
+          data: "0x",
+        })),
+      },
+    ];
+  };
+  it.only("Can handle an underfunded claim", async function () {
+    const initialOutcomeForTargetChannel = createOutcome([
+      ["A", "0x05"],
+      ["B", "0x05"],
+      ["I", "0x0A"],
+    ]);
+    const initialOutcomeForAnotherChannel = createOutcome([["A", "0x03"]]);
+
+    const guarantee: Exit = [
+      {
+        asset: ZERO_ADDRESS,
+        data: "0x",
+        allocations: [
+          {
+            destination: ANOTHER_TARGET_CHANNEL_ADDRESS,
+            amount: "0x03",
+            callTo: MAGIC_VALUE_DENOTING_A_GUARANTEE,
+            data: encodeGuaranteeData(A_ADDRESS, B_ADDRESS),
+          },
+          {
+            destination: TARGET_CHANNEL_ADDRESS,
+            amount: "0x0A", // This should be the total of the allocations in the target channel
+            callTo: MAGIC_VALUE_DENOTING_A_GUARANTEE,
+            data: encodeGuaranteeData(A_ADDRESS, I_ADDRESS, B_ADDRESS),
+          },
+        ],
+      },
+    ];
+
+    const initialHoldings = [BigNumber.from(6)];
+    const exitRequest = [[]];
+
+    const firstClaim = claim(
+      guarantee,
+      initialHoldings,
+      1,
+      initialOutcomeForTargetChannel,
+      exitRequest
+    );
+    // expect(gasEstimate.toNumber()).to.equal(59090);
+
+    const secondClaim = claim(
+      guarantee,
+      initialHoldings,
+      0,
+      initialOutcomeForAnotherChannel,
+      exitRequest
+    );
+
+    expect(firstClaim.updatedTargetOutcome).to.deep.equal(
+      createOutcome([
+        ["A", "0x02"],
+        ["B", "0x05"],
+        ["I", "0x0A"],
+      ])
+    );
+    expect(firstClaim.exit).to.deep.equal(createOutcome([["A", "0x03"]]));
+    expect(firstClaim.updatedHoldings).to.deep.equal(BigNumber.from(3));
+
+    expect(secondClaim.updatedTargetOutcome).to.deep.equal(
+      createOutcome([["A", "0x00"]])
+    );
+    expect(secondClaim.exit).to.deep.equal(createOutcome([["A", "0x03"]]));
+    expect(secondClaim.updatedHoldings).to.deep.equal(BigNumber.from(0));
+  });
 
   it("Can claim with no exit requests", async function () {
     const initialOutcome: Exit = [
@@ -26,9 +112,16 @@ describe("claim (typescript)", function () {
             callTo: ZERO_ADDRESS,
             data: "0x",
           },
+
           {
             destination: B_ADDRESS,
             amount: "0x05",
+            callTo: ZERO_ADDRESS,
+            data: "0x",
+          },
+          {
+            destination: I_ADDRESS,
+            amount: "0x0A",
             callTo: ZERO_ADDRESS,
             data: "0x",
           },
@@ -43,9 +136,9 @@ describe("claim (typescript)", function () {
         allocations: [
           {
             destination: TARGET_CHANNEL_ADDRESS,
-            amount: "0x00",
+            amount: "0x0A", // This should be the total of the allocations in the target channel
             callTo: MAGIC_VALUE_DENOTING_A_GUARANTEE,
-            data: encodeGuaranteeData(B_ADDRESS, A_ADDRESS),
+            data: encodeGuaranteeData(A_ADDRESS, I_ADDRESS, B_ADDRESS),
           },
         ],
       },
@@ -61,6 +154,7 @@ describe("claim (typescript)", function () {
       initialOutcome,
       exitRequest
     );
+    // expect(gasEstimate.toNumber()).to.equal(59090);
 
     expect(updatedHoldings).to.deep.equal([BigNumber.from(0)]);
 
@@ -71,13 +165,19 @@ describe("claim (typescript)", function () {
         allocations: [
           {
             destination: A_ADDRESS,
-            amount: "0x04",
+            amount: "0x00",
             callTo: "0x0000000000000000000000000000000000000000",
             data: "0x",
           },
           {
             destination: B_ADDRESS,
-            amount: "0x00", // TODO: It would be nice if these were stripped out
+            amount: "0x05",
+            callTo: "0x0000000000000000000000000000000000000000",
+            data: "0x",
+          },
+          {
+            destination: I_ADDRESS,
+            amount: "0x09",
             callTo: "0x0000000000000000000000000000000000000000",
             data: "0x",
           },
@@ -91,14 +191,13 @@ describe("claim (typescript)", function () {
         data: "0x",
         allocations: [
           {
-            destination: B_ADDRESS,
+            destination: A_ADDRESS,
             amount: "0x05",
             callTo: "0x0000000000000000000000000000000000000000",
             data: "0x",
           },
-
           {
-            destination: A_ADDRESS,
+            destination: I_ADDRESS,
             amount: "0x01",
             callTo: "0x0000000000000000000000000000000000000000",
             data: "0x",
