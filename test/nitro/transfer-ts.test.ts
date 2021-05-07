22f269a5c60651ec3e1dcb89682f1dfd07eaa638
geoknee <georgeknee@googlemail.com>
2021-05-07 19:54:23 +0100

    add expectation to transfer-ts test

--- test/nitro/transfer-ts.test.ts
+++ test/nitro/transfer-ts.test.ts
@@ -1,5 +1,6 @@
 const { expect } = require("chai");
 import { BigNumber } from "@ethersproject/bignumber";
+import { MAGIC_VALUE_DENOTING_A_GUARANTEE } from "../../nitro-src/nitro-types";
 import { transfer } from "../../nitro-src/transfer";
 import { Exit } from "../../src/types";
 
@@ -72,5 +73,36 @@ describe("transfer (typescript)", function () {
         ],
       },
     ]);
+  });
+
+  it("Reverts if the initialOutcome is a guarantee", async function () {
+    const initialHoldings = [BigNumber.from(6)];
+
+    const guarantee: Exit = [
+      {
+        asset: "0x0000000000000000000000000000000000000000",
+        data: "0x",
+        allocations: [
+          {
+            destination: "0x96f7123E3A80C9813eF50213ADEd0e4511CB820f",
+            amount: "0x05",
+            callTo: MAGIC_VALUE_DENOTING_A_GUARANTEE,
+            data: "0x",
+          },
+          {
+            destination: "0x53484E75151D07FfD885159d4CF014B874cd2810",
+            amount: "0x05",
+            callTo: MAGIC_VALUE_DENOTING_A_GUARANTEE,
+            data: "0x",
+          },
+        ],
+      },
+    ];
+
+    const exitRequest = [[]];
+
+    await expect(() =>
+      transfer(guarantee, initialHoldings, exitRequest)
+    ).to.throw();
   });
 });
