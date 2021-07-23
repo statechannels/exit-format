5d50c10eaeb74b131ced9ac4901e4417b2ec37a6
Mike Kerzhner <mikerzhner@gmail.com>
2021-07-23 12:27:17 -0600

    refactor: rename variables from guarantees to allocations

--- nitro-src/claim.ts
+++ nitro-src/claim.ts
@@ -20,14 +20,14 @@ import _ from "lodash";
  */
 /**
  * @dev Computes the new outcome that should be stored against a target channel after a claim is made on its guarantor.
- * @param guarantees the outcome containing at least one guarantee(s) which will be claimed for each asset.
+ * @param allocations the outcome containing at least one guarantee(s) which will be claimed for each asset.
  * @param holdings initial quantity of each asset held on chain for the guarantor channel. Order matches that of initialGuaranteeOutcome.
  * @param targetChannelIndex the index of the guarantee in the list of guarantees for the given asset -- equivalent to declaring a target channel
  * @param targetOutcome initial outcome stored on chain for the target channel.
  * @param exitRequest list of indices expressing which destinations in the allocation should be paid out for each asset.
  */
 export function claim(
-  guarantees: Exit,
+  allocations: Exit,
   holdings: BigNumber[],
   targetChannelIndex: number,
   targetOutcome: Exit,
@@ -38,15 +38,15 @@ export function claim(
   }
 
   // Deep-copy as we will mutating these objects.
-  const afterClaimGuarantee: Exit = _.cloneDeep(guarantees);
+  const afterClaimGuarantee: Exit = _.cloneDeep(allocations);
   const afterClaimHoldings = _.cloneDeep(holdings);
 
   const afterClaimTargetOutcome: Exit = [];
   const afterClaimExits: Exit = [];
 
   // Iterate through every asset
-  for (let assetIndex = 0; assetIndex < guarantees.length; assetIndex++) {
-    const guaranteesForOneAsset = guarantees[assetIndex].allocations;
+  for (let assetIndex = 0; assetIndex < allocations.length; assetIndex++) {
+    const allocationsForOneAsset = allocations[assetIndex].allocations;
     const targetAllocations = targetOutcome[assetIndex].allocations;
 
     const singleAssetExit: SingleAssetExit = {
@@ -55,7 +55,7 @@ export function claim(
     };
 
     if (
-      guaranteesForOneAsset[targetChannelIndex].allocationType !==
+      allocationsForOneAsset[targetChannelIndex].allocationType !==
       AllocationType.guarantee
     ) {
       throw Error("Expected allocation type guarantee");
@@ -63,11 +63,11 @@ export function claim(
 
     // Any allocations before this one have priority on the funds
     // So we must account for that by reducing the surplus
-    const guaranteesBeforeOurs = guaranteesForOneAsset.slice(
+    const allocationsBeforeOurs = allocationsForOneAsset.slice(
       0,
       targetChannelIndex
     );
-    const amountBeforeUs = sumAllocationAmounts(guaranteesBeforeOurs);
+    const amountBeforeUs = sumAllocationAmounts(allocationsBeforeOurs);
     let maxAmountCanPayOut = holdings[assetIndex].sub(amountBeforeUs);
 
     // If there are not enough funds to fund the guarantee, we skip the asset
@@ -75,9 +75,9 @@ export function claim(
       continue;
     }
 
-    maxAmountCanPayOut = min(
+    const maxAmountToPayOut = min(
       maxAmountCanPayOut,
-      BigNumber.from(guaranteesForOneAsset[targetChannelIndex].amount)
+      BigNumber.from(allocationsForOneAsset[targetChannelIndex].amount)
     );
 
     const {
@@ -85,9 +85,9 @@ export function claim(
       afterClaimGuaranteeAmount,
       newExits,
     } = claimOneGuaranteeForOneAsset(
-      guaranteesForOneAsset[targetChannelIndex],
+      allocationsForOneAsset[targetChannelIndex],
       targetAllocations,
-      maxAmountCanPayOut,
+      maxAmountToPayOut,
       exitRequest[assetIndex]
     );
 
