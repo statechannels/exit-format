eadeefbcc729c8701cc264036751002e6860e7c2
geoknee <georgeknee@googlemail.com>
2021-08-03 10:26:03 +0100

    remove nitro-specific code
    and cairo code

--- cairo/cairo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cairo-compile $1 --output $1.json
-
-# firefox "http://localhost:8100" # open the debugger
-cairo-run --program=$1.json --print_output --layout=small --program_input=$2 --tracer
